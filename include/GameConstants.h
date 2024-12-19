7897d2ff50ea156afdc571daa0e3d4f1dba1db49
Onat Ribar <onatribar@proton.me>
2024-12-19 13:22:06 +0300

    global max move (50 turns total)

--- include/GameConstants.h
+++ include/GameConstants.h
@@ -5,5 +5,6 @@ const int EMPTY = 0;
 const int P1_PIECE = 1; // ▲ - AI Agent
 const int P2_PIECE = 2; // ● - Human Player
 const int BOARD_SIZE = 7;
+const int MAX_MOVERULE = 100; // 50 TURNS, adjust if necessary
 
 #endif
