0a9f228abf349da7e8b99b2e5d4796b265727a44
Onat Ribar <onatribar@proton.me>
2024-12-25 00:35:29 +0300

    set max moves to 50

--- include/GameConstants.h
+++ include/GameConstants.h
@@ -5,7 +5,7 @@ constexpr int EMPTY = 0;
 constexpr int P1_PIECE = 1; // ▲ - AI Agent
 constexpr int P2_PIECE = 2; // ● - Human Player
 constexpr int BOARD_SIZE = 7;
-constexpr int MAX_MOVE_RULE = 100; // 50 TURNS, adjust if necessary
+constexpr int MAX_MOVE_RULE = 50; // 25 TURNS, 50 MOVES, adjust if necessary
 
 struct Move {
     int fromRow, fromCol;
